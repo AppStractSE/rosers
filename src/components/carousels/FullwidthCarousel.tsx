ea0dcf44932c66954e6a0294a76c7f271b04e819
Alexander Auermaa <alexander.auermaa@gmail.com>
2024-06-03 00:43:52 +0200

    Fade speed slowed down a bit

--- src/components/carousels/FullwidthCarousel.tsx
+++ src/components/carousels/FullwidthCarousel.tsx
@@ -6,7 +6,7 @@ import EmblaCarousel from "../embla/EmblaCarousel";
 const FullwidthCarousel = () => {
   const OPTIONS: EmblaOptionsType = {
     loop: true,
-    duration: 30,
+    duration: 150,
   };
   const SLIDES = [
     "https://cdn.discordapp.com/attachments/1210235642467057764/1245507005674819726/catering_rosers.png?ex=665d9d8a&is=665c4c0a&hm=f52a90bb02154d813e85089e8790832a14544c4ae5481c1a2cf45490218cfb65&",

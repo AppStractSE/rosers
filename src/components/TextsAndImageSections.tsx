d1af2bd75709b8fd3026553e5f069f483741b2b3
Alexander Auermaa <alexander.auermaa@gmail.com>
2024-06-03 23:17:38 +0200

    Oopsie poopsie

--- src/components/TextsAndImageSections.tsx
+++ src/components/TextsAndImageSections.tsx
@@ -8,7 +8,7 @@ const TextsAndImageSections = () => {
       subTitle: "Who we are",
       text: `We are more than just a culinary team; we are pioneers of  unforgettable dining experiences. With a legacy spanning over 50 years,  our passion for culinary excellence drives us to redefine the art of  dining. Led by our visionary founder Bengt Roser, our team is dedicated  to crafting moments that transcend taste, transforming ordinary  gatherings into extraordinary occasions.
       \nWelcome to a world where every  bite tells a story, and every meal is an unforgettable journey.`,
-      image: "https://placehold.co/600x700.png",
+      image: "/image001.png",
       button: {
         text: "Read more",
         link: "/who-we-are",

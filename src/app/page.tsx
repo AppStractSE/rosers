72f61133be795b63ea19b5d97c8ec8dcdd71cbcf
Alexander Auermaa <alexander.auermaa@gmail.com>
2024-06-03 23:08:44 +0200

    TextAndImage optimizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+import TextsAndImageSections from "@/components/TextsAndImageSections";
 import FullwidthCarousel from "@/components/carousels/FullwidthCarousel";
-import TextAndImageSection from "@/components/sections/TextAndImageSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
@@ -8,30 +8,7 @@ export default function Home() {
       <section className="">Section 3</section>
       <section className="">Section 4</section>
       <section className="">Section 5</section>
-      <TextAndImageSection
-        rtl={false}
-        title="The Rosers"
-        subTitle="Who we are"
-        text={`We are more than just a culinary team; we are pioneers of  unforgettable dining experiences. With a legacy spanning over 50 years,  our passion for culinary excellence drives us to redefine the art of  dining. Led by our visionary founder Bengt Roser, our team is dedicated  to crafting moments that transcend taste, transforming ordinary  gatherings into extraordinary occasions.
-        \nWelcome to a world where every  bite tells a story, and every meal is an unforgettable journey.`}
-        image="https://placehold.co/600x700.png"
-        button={{
-          text: "Read more",
-          link: "/who-we-are",
-        }}
-      />
-      <TextAndImageSection
-        rtl={true}
-        title="The Rosers"
-        subTitle="Who we are"
-        text={`We are more than just a culinary team; we are pioneers of  unforgettable dining experiences. With a legacy spanning over 50 years,  our passion for culinary excellence drives us to redefine the art of  dining. Led by our visionary founder Bengt Roser, our team is dedicated  to crafting moments that transcend taste, transforming ordinary  gatherings into extraordinary occasions.
-        Welcome to a world where every  bite tells a story, and every meal is an unforgettable journey.`}
-        image="https://placehold.co/600x700.png"
-        button={{
-          text: "Read more",
-          link: "/who-we-are",
-        }}
-      />
+      <TextsAndImageSections />
       <section className="w-full">
         <FullwidthCarousel />
       </section>
--- src/components/sections/TextAndImageSection.tsx
+++ src/components/sections/TextAndImageSection.tsx
@@ -18,25 +18,31 @@ const TextAndImageSection = ({ rtl, title, subTitle, text, image, button }: Prop
     <section className="w-full px-4 py-8">
       <div className="mx-auto max-w-screen-2xl">
         <div
-          className={`flex items-center gap-14 justify-between ${
-            rtl ? "flex-row-reverse" : "flex-row"
+          className={`pl-4 border-l border-[#a2866854] lg:p-0 lg:border-0 flex items-center gap-8 lg:gap-14 justify-between ${
+            rtl ? "lg:flex-row-reverse flex-col" : "lg:flex-row flex-col"
           }`}
         >
-          <div className="basis-5/12 rounded-sm border border-[#a28668d7] overflow-hidden">
-            <img src={image} alt="Image" className="w-full h-auto" />
+          <div className="h-[15rem] md:h-[30rem] lg:h-[30rem] xl:h-[45rem] flex flex-col justify-center basis-12/12 lg:basis-5/12 w-full rounded-sm border border-[#a28668d7] overflow-hidden">
+            <img src={image} alt="Image" className="h-full w-full object-cover" />
           </div>
           <div className="basis-7/12">
             <h3 className={`${futuraStd.className} text-xs uppercase text-[#A28668]`}>
               {subTitle}
             </h3>
             <h2 className={`${futuraStd.className} text-4xl mt-2`}>{title}</h2>
-            <p className={`${garamond.className} text-xl tracking-wide font-thin mt-8 whitespace-pre-line`}>
+            <p
+              className={`${garamond.className} text-xl tracking-wide font-thin mt-8 whitespace-pre-line`}
+            >
               {text}
             </p>
-            <div className="flex justify-end">
+            <div className="flex lg:justify-end">
               <Link
                 href={button.link}
-                className={`${futuraStd.className} inline-block ml-auto px-12 py-5 border text-[#a28668d7] transition-all duration-200 ease-in-out border-[#a28668d7] rounded-sm tracking-[0.2em] mt-16 hover:bg-[#A28668] hover:text-[#232323]`}
+                className={`${futuraStd.className}
+                lg:w-fit w-full text-center inline-block py-2.5 lg:px-12
+                lg:py-4 border lg:text-[#A28668] lg:bg-transparent transition-all duration-200 ease-in-out
+                border-[#A28668] rounded-sm tracking-[0.2em] mt-12 lg:mt-16 text-sm lg:text-base bg-[#A28668] text-[#232323]
+                hover:bg-[#A28668] hover:text-[#232323]`}
               >
                 {button.text}
               </Link>

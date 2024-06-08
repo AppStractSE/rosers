d3b82c6c7bdea2d8736e70fa35af3eb216fed938
Alexander Auermaa <alexander.auermaa@gmail.com>
2024-06-08 14:52:51 +0200

    Hero section + some colors added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        xxxs: [".5rem", "1"],
+        xxs: [".625rem", "1"],
+      },
       colors: {
-        primary: "#F5CEA4",
-        background: "#232323",
+        gold: "#f5cea4",
+        brass: "#af8f6e",
+        charcoal: {
+          50: "#707070",
+          100: "#666666",
+          200: "#5C5C5C",
+          300: "#525252",
+          400: "#474747",
+          500: "#3D3D3D",
+          600: "#333333",
+          700: "#232323",
+          800: "#1F1F1F",
+          850: "#1A1A1A",
+          900: "#141414",
+          950: "#0A0A0A",
+        },
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",

7ff6e73478525c0bbbd4153043b8193e52bbb03b
Alexander Auermaa <alexander.auermaa@gmail.com>
2025-10-12 22:15:59 +0200

    catch locales

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: "/:locale/links",
+        destination: "https://bento.me/rosers",
+        permanent: true,
+      },
+      {
         source: "/links",
         destination: "https://bento.me/rosers",
         permanent: true,

4f52258bdf80a01596ed6d509675b7abfb0506a8
Alexander Auermaa <alexander.auermaa@gmail.com>
2025-10-12 22:10:09 +0200

    redirect to bento

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/links",
+        destination: "https://bento.me/rosers",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
